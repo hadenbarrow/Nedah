850832d46150bfb924b1e92a344c4c19b2b82570
Haden <Haden@10.0.0.172>
2019-03-11 15:43:24 -0700

    Made changes to move generator.

--- Nedah/src/main/MoveGenerator.java
+++ Nedah/src/main/MoveGenerator.java
@@ -21,7 +21,7 @@ public class MoveGenerator {
 			for(int j = 0; j < board.length; j++){
 				
 				if(board[i][j] == PIECES.CPAWN.value){
-					if(!checkCollision(board, i+1, j) && isOnBoard(i+1, j)){
+					if(!checkCollisionWithComputerPiece(board, i+1, j) && isOnBoard(i+1, j)){
 						moves.add(createMoveStringForComputer(i, j, i+1, j));
 					}
 				}
@@ -42,14 +42,31 @@ public class MoveGenerator {
 	public void generatePlayerMoves(int[][] board){}
 	
 	public String createMoveStringForComputer(int oldi, int oldj, int newi, int newj){
-		return "";
+		String oldColumn = getCorrespondingColumn(oldj);
+		String newColumn = getCorrespondingColumn(newj);
+		return oldColumn + oldi + newColumn + newi; 
+	}
+	
+	public String getCorrespondingColumn(int n){
+		if(n > 8 || n < 1){
+			return "";
+		}
+		if(n == 1){return "a";} 
+		else if(n == 2){return "b";}
+		else if(n == 3){return "c";}
+		else if(n == 4){return "d";}
+		else if(n == 5){return "e";}
+		else if(n == 6){return "f";}
+		else if(n == 7){return "g";}
+		else if(n == 8){return "h";}
+		return "couldn't figure out move string";
 	}
 	
 	private boolean isOnBoard(int i, int j){
 		return (i < 6 && j < 8);
 	}
 	
-	private boolean checkCollision(int[][] board, int i, int j){
+	private boolean checkCollisionWithComputerPiece(int[][] board, int i, int j){
 		if(board[i][j] == PIECES.CPAWN.value || board[i][j] == PIECES.CHORSE.value 
 				|| board[i][j] == PIECES.CBISHOP.value || board[i][j] == PIECES.CKING.value){
 			return true;
