a7aeed4d1470f955721dcbb8fa77f84a224bbb50
Haden <Haden@10.0.0.172>
2019-03-12 17:27:14 -0700

    Started work on minimax

--- Nedah/src/main/CongressChess.java
+++ Nedah/src/main/CongressChess.java
@@ -19,7 +19,7 @@ public class CongressChess {
 		run();
 	}
 	
-	public void init(){
+	private void init(){
 		scanner = new Scanner(System.in);
 		gameBoardInitializer = new GameBoardInitializer();
 		board = gameBoardInitializer.generateNewBoard();
@@ -30,7 +30,7 @@ public class CongressChess {
 		run = true;
 	}
 	
-	public void intro(){
+	private void intro(){
 		System.out.println("Welcome to Congress Chess.");
 		System.out.println("You will be playing against an AI called Nedah.");
 		System.out.println("Who would you like to make the first move? Enter 1 for human or 2 for computer: ");
@@ -49,7 +49,7 @@ public class CongressChess {
 			if(computerTurn){
 				String move = makeComputerMove();
 				updateBoardState(move);
-				checkGameOver();
+				checkGameOver(board);
 				displayBoard();
 				System.out.println("My move is : " + move);
 				computerTurn = false;
@@ -61,7 +61,7 @@ public class CongressChess {
 					illegalMoveFlag = checkMove(userMove);
 				}
 				updateBoardState(userMove);
-				checkGameOver();
+				checkGameOver(board);
 				displayBoard();
 				computerTurn = true;
 			}
@@ -69,7 +69,7 @@ public class CongressChess {
 			if(computerTurn){
 				String move = makeComputerMove();
 				updateBoardState(move);
-				checkGameOver();
+				checkGameOver(board);
 				displayBoard();
 				System.out.println("My move is : " + move);
 				computerTurn = false;
@@ -81,7 +81,7 @@ public class CongressChess {
 					illegalMoveFlag = checkMove(userMove);
 				}
 				updateBoardState(userMove);
-				checkGameOver();
+				checkGameOver(board);
 				displayBoard();
 				computerTurn = true;
 			}
@@ -99,14 +99,14 @@ public class CongressChess {
 		}
 	}
 	
-	public String promptUserForMove(){
+	private String promptUserForMove(){
 		String validMoves = getValidMoves();
 		System.out.println("Valid moves for player: " + validMoves);
 		System.out.println("Enter your move: ");
 		return scanner.nextLine();
 	}
 	
-	public boolean checkMove(String move){
+	private boolean checkMove(String move){
 		List<String> playerMoves = moveGenerator.generateMoves("player", board);
 		if(!playerMoves.contains(move)){
 			System.err.println("That move is not valid");
@@ -116,11 +116,11 @@ public class CongressChess {
 		return false;
 	}
 	
-	public void updateBoardState(String move){
+	private void updateBoardState(String move){
 		board = gameBoardUpdater.updateBoard(board, move);
 	}
 	
-	public void checkGameOver(){
+	private boolean checkGameOver(int[][] board){
 		int computerKings = 0;
 		int playerKings = 0;
 		
@@ -137,14 +137,17 @@ public class CongressChess {
 		if(computerKings == 0){
 			playerWins = true;
 			run = false;
+			return true;
 		}
 		else if(playerKings == 0){
 			computerWins =  true;
 			run = false;
+			return true;
 		}
+		return false;
 	}
 	
-	public String makeComputerMove(){
+	private String makeComputerMove(){
 		List<String> computerMoves = moveGenerator.generateMoves("computer", board);
 		System.out.print("Valid moves for Nedah: ");
 		
@@ -157,17 +160,70 @@ public class CongressChess {
 		return computerMoves.get(selection);
 	}
 	
-	public void displayBoard(){
+	private void displayBoard(){
 		gameBoardDisplay.displayBoard(board);
 	}
 	
-	public String getValidMoves(){
+	private String getValidMoves(){
 		List<String> playerMoves = moveGenerator.generateMoves("player", board);
 		String temp ="";
 		for(String s : playerMoves){
 			temp += s + " ";
 		}
 		return temp;
+	}
+	
+	
+	private int minimax(int[][] board, int depth, boolean maximizingPlayer){
+		if(depth == 0 || checkGameOver(board)){
+			return eval(board);
+		}
+		return 0;
+	}
+	
+	private int eval(int[][] board){
+		int computerMaterial = getComputerMaterial(board);
+		int playerMaterial = getPlayerMaterial(board);
+		
+		return computerMaterial - playerMaterial;
+	}
+	
+	private int getComputerMaterial(int[][] board){
+		int computerMaterial = 0;
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board[0].length; j++){
+				int piece = board[i][j];
+				if(piece == PIECES.CPAWN.value){
+					computerMaterial += 1;
+				}
+				else if(piece == PIECES.CBISHOP.value){
+					computerMaterial += 3;
+				}
+				else if(piece == PIECES.CHORSE.value){
+					computerMaterial += 3;
+				}
+			}
+		}
+		return computerMaterial;
+	}
+	
+	private int getPlayerMaterial(int[][] board){
+		int playerMaterial = 0;
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board[0].length; j++){
+				int piece = board[i][j];
+				if(piece == PIECES.PAWN.value){
+					playerMaterial += 1;
+				}
+				else if(piece == PIECES.BISHOP.value){
+					playerMaterial += 3;
+				}
+				else if(piece == PIECES.HORSE.value){
+					playerMaterial += 3;
+				}
+			}
+		}
+		return playerMaterial;
 	}
 	
 	
