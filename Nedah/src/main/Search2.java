3d554a68eab509d885506c4c4733dd06b6b39c8a
Haden Barrow <hadenbarrow@csus.edu>
2019-03-21 12:40:28 -0700

    Added an "opposing AI" to test Nedah against. It's simply minimax with AB pruning

---
+++ Nedah/src/main/Search2.java
@@ -0,0 +1,209 @@
+package main;
+
+import java.util.List;
+
+public class Search2{
+	private String threadMove;
+	private GameBoardUpdater gameBoardUpdater;
+	private MoveGenerator moveGenerator;
+	private int leafNodes;
+	private int maxDepth;
+	private int[][] board;
+	private boolean searchWasCutOff;
+	
+	public Search2(int[][] board) {
+		this.board = board;
+		leafNodes = 0;
+		gameBoardUpdater = new GameBoardUpdater();
+		moveGenerator = new MoveGenerator();
+		searchWasCutOff = false;
+	}
+	
+	public String getComputerMove(long startTime, int maxDepth){
+		this.maxDepth = maxDepth;
+		int[][] newBoard = copyBoard(board);
+		int best = -5000, depth = 0, score = 0;
+		String move = "";
+		List<String> computerMoves = moveGenerator.generateMoves("player", newBoard);
+		for(String s : computerMoves) {
+			newBoard = copyBoard(board);
+			newBoard = gameBoardUpdater.updateBoard(newBoard, s); //make move
+			score = min(newBoard, depth+1, -5000, 5000, startTime);
+			if(score >= best) {
+				best = score;
+				move = s;
+			}
+		}
+		return move;
+	}
+	
+	private int min(int[][] board, int depth, int a, int b, long startTime) {
+		int best = 5000, score = 0;
+		
+		if(System.currentTimeMillis() - startTime > 4990) {
+			searchWasCutOff = true;
+			return best;
+		}
+		
+		if(checkForWinner(board, depth) != -1) {return checkForWinner(board, depth);}
+		if(depth == maxDepth) {return minEval(board, depth);}
+		
+		List<String> playerMoves = moveGenerator.generateMoves("computer", board);
+		for(String s : playerMoves) {
+			int[][] oldBoard = copyBoard(board);
+			board = gameBoardUpdater.updateBoard(board, s);
+			score = max(board, depth+1, a, b, startTime);
+			if(score < best) {
+				best = score;
+			}
+			if(score <= a) {
+				return best; //prune
+			}
+			if(score < b) {
+				b = score;
+			}
+			board = oldBoard;
+		}
+		return best;
+	}
+	
+	private int max(int[][] board, int depth, int a, int b, long startTime) {
+		int best = -5000, score = 0;
+		
+		if(System.currentTimeMillis() - startTime > 4990) {
+			searchWasCutOff = true;
+			return best;
+		}
+		
+		if(checkForWinner(board, depth) != -1) {return checkForWinner(board, depth);}
+		if(depth == maxDepth) {return maxEval(board, depth);}
+		
+		List<String> computerMoves = moveGenerator.generateMoves("player", board);
+		for(String s : computerMoves) {
+			int[][] oldBoard = copyBoard(board);
+			board = gameBoardUpdater.updateBoard(board, s);
+			score = min(board, depth+1, a, b, startTime);
+			if(score > best) {
+				best = score;
+			}
+			if(score >= b) {
+				return best; //prune
+			}
+			if(score > a) {
+				a = score;
+			}
+			board = oldBoard;
+		}
+		return best;
+	}
+	
+	private int checkForWinner(int[][] board, int depth) {
+		int computerKings = 0;
+		int playerKings = 0;
+		
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board[0].length; j++){
+				if(board[i][j] == PIECES.KING.value){
+					playerKings++;
+				}
+				if(board[i][j] == PIECES.CKING.value){
+					computerKings++;
+				}
+			}
+		}
+		if(computerKings == 0) {
+			return 5000 + depth;
+		}
+		else if(playerKings == 0) {
+			return -5000 - depth;
+		} else {
+			return -1;
+		}
+	}
+	
+	private int minEval(int[][] board, int depth){
+		int computerMaterial = getComputerMaterial(board);
+		int playerMaterial = getPlayerMaterial(board);
+		
+		leafNodes++;
+		return playerMaterial - (computerMaterial - depth);
+	}
+	
+	private int maxEval(int[][] board, int depth){
+		int computerMaterial = getComputerMaterial(board);
+		int playerMaterial = getPlayerMaterial(board);
+		
+		leafNodes++;
+		return playerMaterial - (computerMaterial + depth);
+	}
+	
+	private int getComputerMaterial(int[][] board){
+		int computerMaterial = 0;
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board[0].length; j++){
+				int piece = board[i][j];
+				if(piece == PIECES.CPAWN.value){
+					computerMaterial += 1;
+				}
+				else if(piece == PIECES.CBISHOP.value){
+					computerMaterial += 3;
+				}
+				else if(piece == PIECES.CHORSE.value){
+					computerMaterial += 3;
+				}
+				else if(piece == PIECES.CKING.value) {
+					computerMaterial += 10;
+				}
+			}
+		}
+		return computerMaterial;
+	}
+	
+	private int getPlayerMaterial(int[][] board){
+		int playerMaterial = 0;
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board[0].length; j++){
+				int piece = board[i][j];
+				if(piece == PIECES.PAWN.value){
+					playerMaterial += 1;
+				}
+				else if(piece == PIECES.BISHOP.value){
+					playerMaterial += 3;
+				}
+				else if(piece == PIECES.HORSE.value){
+					playerMaterial += 3;
+				}
+				else if(piece == PIECES.KING.value) {
+					playerMaterial += 10;
+				}
+			}
+		}
+		return playerMaterial;
+	}
+	
+	private int[][] copyBoard(int[][] board) {
+		int[][] copy = new int[6][8];
+		for(int i = 0; i < board.length; i++) {
+			for(int j = 0; j < board[0].length; j++) {
+				copy[i][j] = board[i][j];
+			}
+		}
+		return copy;
+	}
+	
+	public String getThreadsMove() {
+		return threadMove;
+	}
+	
+	public int getDepth() {
+		return maxDepth;
+	}
+	
+	public int getLeafNodes() {
+		return leafNodes;
+	}
+	
+	public boolean getSearchWasCutOff() {
+		return searchWasCutOff;
+	}
+}
